9713dd10c55e0c961a7a4f24cd51c46fbca20352
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-10-13 02:13:48 -0300

    added neargas

--- Light/scripts/create-database.sh
+++ Light/scripts/create-database.sh
@@ -2,8 +2,22 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER catan_leaderboards CREATEDB PASSWORD '$RAILS_PASSWORD';
-    CREATE USER owncloud CREATEDB PASSWORD '$OWNCLOUD_PASSWORD';   
-    CREATE DATABASE owncloud;
-    GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud; 
+
+	DO \$\$ 
+	BEGIN
+		IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'catan_leaderboards') THEN
+      			CREATE USER catan_leaderboards CREATEDB PASSWORD '$RAILS_PASSWORD';
+    		END IF;
+		IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'Bencinera') THEN
+			CREATE USER "Bencinera" CREATEDB PASSWORD '$BENCINERA_DATABASE_PASSWORD';
+		END IF;
+			IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'catan_leaderboards') THEN
+		CREATE USER owncloud CREATEDB PASSWORD '$OWNCLOUD_PASSWORD';  
+			END IF;
+		IF NOT EXISTS (SELECT FROM   pg_database WHERE datname = 'owncloud') THEN
+			CREATE DATABASE owncloud; 
+		END IF;
+		GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud; 
+	END 
+	\$\$
 EOSQL
