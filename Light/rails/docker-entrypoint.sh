f01e4cacbb2ac7011c6c311a70cddec63e235c86
Teodoro Hochfärber <teohoch2@gmail.com>
2017-10-13 02:16:29 -0300

    Changes

--- Light/rails/docker-entrypoint.sh
+++ Light/rails/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 sleep 2
 echo "Installing and updating gems"
 
-bundle install && bundle update
+#bundle install && bundle update
 
 echo "Creating database if it doesn't exist and runing migrations"
 rake db:migrate 2>/dev/null || rake db:setup
