f2d118943a17f7ac14767473f17616a0427b811d
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-10-25 02:05:13 -0300

    again

--- NearGass-Testing/neargas/docker-entrypoint.sh
+++ NearGass-Testing/neargas/docker-entrypoint.sh
@@ -5,6 +5,7 @@ cp -fr /database.yml /neargass/config/database.yml
 
 bundle install  --jobs 3 --quiet && bundle update  --jobs 3 --quiet
 
+
 rake db:migrate 2>/dev/null || rake db:setup
 
 exec "$@"
