387f441188f2f9a8880624f263d96b8843ef9bdf
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-10-25 00:45:33 -0300

    Data

--- NearGass-Testing/psql/create-database.sh
+++ NearGass-Testing/psql/create-database.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-
 	DO \$\$
 	BEGIN
 		IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'Bencinera') THEN
