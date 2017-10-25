839ed1247c9996a3d468c7e57778f5a17b762fe6
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-10-24 23:37:30 -0300

    Redid compose

---
+++ NearGass-Testing/psql/create-database.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+
+	DO \$\$
+	BEGIN
+		IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'catan_leaderboards') THEN
+      			CREATE USER catan_leaderboards CREATEDB PASSWORD '$RAILS_PASSWORD';
+    		END IF;
+		IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'Bencinera') THEN
+			CREATE USER "Bencinera" CREATEDB PASSWORD '$BENCINERA_DATABASE_PASSWORD';
+		END IF;
+			IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'catan_leaderboards') THEN
+		CREATE USER owncloud CREATEDB PASSWORD '$OWNCLOUD_PASSWORD';
+			END IF;
+		IF NOT EXISTS (SELECT FROM   pg_database WHERE datname = 'owncloud') THEN
+			CREATE DATABASE owncloud;
+		END IF;
+		GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud;
+	END
+	\$\$
+EOSQL
