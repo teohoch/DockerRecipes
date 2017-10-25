5817ec96bd5d4d5a2018d513a9203d12152ec5e8
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-10-25 02:00:36 -0300

    Forgot the S

--- NearGass-Testing/neargas/docker-entrypoint.sh
+++ NearGass-Testing/neargas/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 cp -fr /database.yml /neargass/config/database.yml
 
-bundle install  --job 3 --quiet && bundle update  --job 3 --quiet
+bundle install  --jobs 3 --quiet && bundle update  --jobs 3 --quiet
 
 rake db:migrate 2>/dev/null || rake db:setup
 
