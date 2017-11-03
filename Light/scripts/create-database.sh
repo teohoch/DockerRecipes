7009b5805da8186e40631c38b3ad3403b770b00c
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-11-03 01:30:57 -0300

    added configuration for ssl for neargas

--- Light/scripts/create-database.sh
+++ Light/scripts/create-database.sh
@@ -3,21 +3,21 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 
-	DO \$\$ 
+	DO \$\$
 	BEGIN
 		IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'catan_leaderboards') THEN
       			CREATE USER catan_leaderboards CREATEDB PASSWORD '$RAILS_PASSWORD';
     		END IF;
 		IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'Bencinera') THEN
-			CREATE USER "Bencinera" CREATEDB PASSWORD '$BENCINERA_DATABASE_PASSWORD';
+			CREATE USER "Bencinera" SUPERUSER PASSWORD '$BENCINERA_DATABASE_PASSWORD';
 		END IF;
 			IF NOT EXISTS (SELECT FROM   pg_catalog.pg_user WHERE  usename = 'catan_leaderboards') THEN
-		CREATE USER owncloud CREATEDB PASSWORD '$OWNCLOUD_PASSWORD';  
+		CREATE USER owncloud CREATEDB PASSWORD '$OWNCLOUD_PASSWORD';
 			END IF;
 		IF NOT EXISTS (SELECT FROM   pg_database WHERE datname = 'owncloud') THEN
-			CREATE DATABASE owncloud; 
+			CREATE DATABASE owncloud;
 		END IF;
-		GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud; 
-	END 
+		GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud;
+	END
 	\$\$
 EOSQL
